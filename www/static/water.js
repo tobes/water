db3dc082cbfa401505028e15dc8109a731691556
Toby Dacre <toby.junk@gmail.com>
2022-12-09 23:20:55 +0000

    js clean-ups

--- www/static/water.js
+++ www/static/water.js
@@ -1,9 +1,11 @@
+/*jslint browser:true, esnext:true*/
+
 var update_timeout;
-var last_update = 0;
+let last_update = 0;
+
 const UPDATE_INTERVAL = 1000 * 60 * 15;
 
-function update(data)
-{
+function update(data) {
 
   //console.log(data);
   var w = data.weather.state;
@@ -13,7 +15,7 @@ function update(data)
   var depth = sensor.depth;
   var volume = sensor.volume;
   var pump = data['pump 1'];
-  var pump_state = pump.state
+  var pump_state = pump.state;
 
   const options = {
     weekday: "long",
@@ -27,132 +29,103 @@ function update(data)
   let message_time = new Date(data.message_time).toLocaleString('en-GB',
     options);
 
-  document.getElementById("icon").src = "/static/img/" + icon + ".png"
+  document.getElementById("icon").src = "/static/img/" + icon + ".png";
   document.getElementById("temp").innerText = temp + ' °C';
   document.getElementById("depth").innerText = depth + ' mm';
   document.getElementById("volume").innerText = volume + ' litres';
   document.getElementById("pump_state").innerText = 'Pump ' + pump_state;
   document.getElementById("message_time").innerText = message_time;
-  return;
+
   var json = JSON.stringify(data, undefined, 4);
   json = json.replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g,
     '&gt;');
   json = json.replace(
     /("(\\u[a-zA-Z0-9]{4}|\\[^u]|[^\\"])*"(\s*:)?|\b(true|false|null)\b|-?\d+(?:\.\d*)?(?:[eE][+\-]?\d+)?)/g,
-    function(match)
-    {
+    function(match) {
       var cls = 'j_number';
-      if (/^"/.test(match))
-      {
-        if (/:$/.test(match))
-        {
+      if (/^"/.test(match)) {
+        if (/:$/.test(match)) {
           cls = 'j_key';
-        }
-        else
-        {
+        } else {
           cls = 'j_string';
         }
-      }
-      else if (/true|false/.test(match))
-      {
+      } else if (/true|false/.test(match)) {
         cls = 'j_boolean';
-      }
-      else if (/null/.test(match))
-      {
+      } else if (/null/.test(match)) {
         cls = 'j_null';
       }
       return '<span class="' + cls + '">' + match + '</span>';
     });
-  document.getElementById("message_raw").innerHTML = json;
+  //document.getElementById("message_raw").innerHTML = json;
 }
 
 
-function next_update()
-{
-  var now = new Date().getTime()
-  if (now - last_update >= UPDATE_INTERVAL)
-  {
+function next_update() {
+  var now = new Date().getTime();
+  if (now - last_update >= UPDATE_INTERVAL) {
     return 0;
   }
-  return UPDATE_INTERVAL - now % UPDATE_INTERVAL
+  return UPDATE_INTERVAL - now % UPDATE_INTERVAL;
 }
 
-function request(url, callback)
-{
-  url = 'https://tollington.duckdns.org/' + url;
-  //url = 'http://192.168.1.7:5000/' + url;
+function request(url, callback) {
   // Creating Our XMLHttpRequest object
   var xhr = new XMLHttpRequest();
   // Making our connection
   xhr.open("GET", url, true);
   // function execute after request is successful
-  xhr.onreadystatechange = function()
-  {
-    if (this.readyState == 4 && this.status == 200)
-    {
+  xhr.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
       callback(JSON.parse(this.responseText));
     }
-  }
+  };
   // Sending our request
   xhr.send();
 }
 
-function update_status()
-{
-  if (update_timeout)
-  {
+function update_status() {
+  if (update_timeout) {
     clearTimeout(update_timeout);
   }
   request('/status', update);
 
-  last_update = new Date().getTime()
-  update_timeout = setTimeout(update_status, next_update())
+  last_update = new Date().getTime();
+  update_timeout = setTimeout(update_status, next_update());
 }
 
-document.onvisibilitychange = () =>
-{
-  if (document.visibilityState === 'hidden')
-  {
-    if (update_timeout)
-    {
+document.onvisibilitychange = () => {
+  if (document.visibilityState === 'hidden') {
+    if (update_timeout) {
       clearTimeout(update_timeout);
     }
 
-  }
-  else
-  {
+  } else {
 
-    update_timeout = setTimeout(update_status, next_update())
+    update_timeout = setTimeout(update_status, next_update());
   }
 };
 document.getElementById("main").addEventListener("click", update_status);
 
-function make_value(value, col_info)
-{
-  if (col_info.type === 'date')
-  {
+function make_value(value, col_info) {
+  if (col_info.type === 'date') {
     value = new Date(value).toLocaleDateString();
   }
 
-  if (col_info.type === 'time')
-  {
+  if (col_info.type === 'time') {
     value = value.substring(0, 5);
   }
 
-  if (col_info.type === 'float')
-  {
-    value = value.toFixed(2)
+  if (col_info.type === 'float') {
+    value = value.toFixed(2);
   }
 
-  if (col_info.type === 'seconds')
-  {
+  if (col_info.type === 'seconds') {
     var minutes = Math.floor(value / 60);
     var seconds = value - minutes * 60;
     value = "" + minutes + ":" + (seconds < 10 ? "0" : "") + seconds;
   }
 
-  if (col_info.units)
-  {
+  if (col_info.units) {
     value += col_info.units;
   }
   return value;
@@ -160,17 +133,15 @@ function make_value(value, col_info)
 }
 
 
-function update_stats(data)
-{
-  const cols = data.cols
-  const values = data.values
+function update_stats(data) {
+  const cols = data.cols;
+  const values = data.values;
   let table = document.createElement('table');
   let thead = document.createElement('thead');
   table.appendChild(thead);
   let tr = document.createElement('tr');
   thead.appendChild(tr);
-  for (const col of cols)
-  {
+  for (const col of cols) {
     let th = document.createElement('th');
     th.innerText = col.title;
     th.setAttribute('class', col.type);
@@ -178,12 +149,10 @@ function update_stats(data)
   }
   let tbody = document.createElement('tbody');
   table.appendChild(tbody);
-  values.forEach(row =>
-  {
+  values.forEach(row => {
     let tr = document.createElement('tr');
     tbody.appendChild(tr);
-    for (let i = 0; i < cols.length; i++)
-    {
+    for (let i = 0; i < cols.length; i++) {
       let value = make_value(row[i], cols[i]);
       let td = document.createElement('td');
       td.setAttribute('class', cols[i].type);
@@ -196,8 +165,7 @@ function update_stats(data)
   document.body.appendChild(table);
 }
 
-function stats()
-{
+function stats() {
   request('/stats', update_stats);
   request('/stats_pump', update_stats);
   request('/stats_weather', update_stats);
