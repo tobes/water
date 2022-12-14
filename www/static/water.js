9c4400a226eda214c83eefd86df90b573a85ee7b
Toby Dacre <toby.junk@gmail.com>
2022-12-14 09:47:57 +0000

    remove json formatter

--- www/static/water.js
+++ www/static/water.js
@@ -66,31 +66,6 @@ function update(data, automated) {
   }
 }
 
-function show_json(data) {
-  var json = JSON.stringify(data, undefined, 4);
-  json = json.replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g,
-    '&gt;');
-  json = json.replace(
-    /("(\\u[a-zA-Z0-9]{4}|\\[^u]|[^\\"])*"(\s*:)?|\b(true|false|null)\b|-?\d+(?:\.\d*)?(?:[eE][+\-]?\d+)?)/g,
-    function(match) {
-      var cls = 'j_number';
-      if (/^"/.test(match)) {
-        if (/:$/.test(match)) {
-          cls = 'j_key';
-        } else {
-          cls = 'j_string';
-        }
-      } else if (/true|false/.test(match)) {
-        cls = 'j_boolean';
-      } else if (/null/.test(match)) {
-        cls = 'j_null';
-      }
-      return '<span class="' + cls + '">' + match + '</span>';
-    }
-  );
-  document.getElementById('message_raw').innerHTML = json;
-}
-
 
 function request(url, callback, payload) {
   url = 'https://tollington.duckdns.org/' + url;
